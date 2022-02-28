cff84b526587b35b5a91edc9eee8d39c5f4d4982
Nikita Shymberg <nikitashymberg@gmail.com>
2022-02-28 14:42:41 -0800

    working on creating the structure

---
+++ src/line_plot.py
@@ -0,0 +1,24 @@
+import altair as alt
+import pandas as pd
+
+
+def generate_line_plot(data: pd.DataFrame):
+    """"
+    Generates the line chart for the dashboard
+
+    Parameters
+    ----------
+    data : pandas dataframe
+        The dataframe that contains the data to plot.
+
+    Returns
+    -------
+    chart : altair Chart
+        The generated line chart
+    """
+    ...  # TODO: implement
+
+
+if __name__ == "__main__":
+    data = None  # TODO: wait for Brandon to make the data
+    generate_line_plot(data)
