2f707a08a2d1ae97238990a65b64069e6de1b32d
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-03-01 15:48:21 -0800

    add: script to generate bubble chart with docstring

---
+++ src/bubble_chart.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import altair as alt
+
+def bubble_chart_gen(data, x, y):
+    """
+    Generate the bubble chart.
+
+    Parameters
+    ----------
+    data : pandas dataframe
+        Dataframe containing the data to plot.
+    x : string
+        Column name to plot on the x-axis.
+    y : string
+        Column name to plot on the y-axis.
+    
+    Results
+    -------
+    chart : html of altair Chart
+        The generated bubble chart converted to html
+    """
