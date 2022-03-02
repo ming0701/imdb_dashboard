105413ac377c64a0c3ade2468407aa1ee4c75946
Brandon Lam <ming0701@gmail.com>
2022-03-01 18:01:05 -0800

    create boxplot

---
+++ src/boxplot.py
@@ -0,0 +1,34 @@
+import altair as alt
+import pandas as pd
+
+
+def generate_boxplot(data: pd.DataFrame, region: str):
+    """"
+    Generates the boxplot for the dashboard
+
+    Parameters
+    ----------
+    data : pandas dataframe
+        The dataframe that contains the data to plot.
+
+    Returns
+    -------
+    chart : html of altair Chart
+        The generated boxplot converted to html
+    """
+
+    # Prepare data for boxplot
+    data = data.drop(['primaryName','Unnamed: 0','nconst'], axis=1)
+    data = data.drop_duplicates()
+    
+    # filter by genres and rating
+    #data = data[(data['genres'] == genres) &
+    #            (data['averageRating'] >= rating) ]
+    
+    # Create Boxplot
+    chart = alt.Chart(data, title = 'Distribution of movies by genre').mark_boxplot().encode(
+        x=alt.X('genres', axis=alt.Axis(title="Genre")),
+        y=alt.Y('averageRating', axis=alt.Axis(title="IMDB Rating")),
+        color='region')
+
+    return chart.to_html()
